269abfb4f2a1c8568c0282895ab05617d9bc2d24
Goutam96801 <gautamkumar96801@gmail.com>
2023-03-28 03:55:30 +0530

    Creating route

--- index.js
+++ index.js
@@ -2,10 +2,38 @@ const http = require('http');
 
 const port = 8081; //Local port number
 
+const toDoList = ["Complete Node Byte", "Play Cricket"];
+
+
 http.createServer((request, response) => {
-    response.writeHead(200, { 'Content-Type': 'text/html' });
-    response.write('<h1>Hello, Welcome to my server</h1>');
+    // response.writeHead(200, { 'Content-Type': 'text/html' });
+    // response.write('<h1>Hello, Welcome to my server</h1>');
+    // response.end();
+    const { method, url } = request;
+
+    if (url === "/todos") {
+        if (method === "GET") {
+            response.writeHead(200,{ "Content-Type": "text/html" });
+            response.write(toDoList.toString());
+        } else if (method === "POST") {
+            let body = "";
+            request.on('error', (err) => {
+                console.error(err)
+            }).on('data', (chunk) => {
+                body += chunk;
+                console.log(chunk);
+            }).on('end', () => {
+                body = JSON.parse(body);
+                console.log("data: ", body);
+            })
+        }
+        else {
+            response.writeHead(501);
+        }
+    }
+    // console.log(method, url);
     response.end();
+
 
 })
 .listen(port, () => { //callback function
